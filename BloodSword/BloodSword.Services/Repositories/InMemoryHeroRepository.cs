584c641c4845f499d0569c4ce04a98987f943693
Georgi Hristov <Georgi.dh@gmail.com>
2025-08-06 00:23:06 +0300

    Added InMemoryHeroRepository and registered it for dependency injection.

---
+++ BloodSword/BloodSword.Services/Repositories/InMemoryHeroRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodSword.Services.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface InMemoryHeroRepository
+    {
+    }
+}
