8c9d5bbb63d9d2d779f8cc23cce86d1e84ddfab1
Georgi Hristov <Georgi.dh@gmail.com>
2025-08-06 21:30:48 +0300

    Fixed the issue with wepons impact to total damage

--- BloodSword/BloodSword.Domain/Characters/Hero.cs
+++ BloodSword/BloodSword.Domain/Characters/Hero.cs
@@ -4,6 +4,9 @@ namespace BloodSword.Domain.Characters
 {
     public class Hero : Character
     {
+        public int TotalAttackSkill => AttackSkill + Inventory.OfType<Weapon>().Sum(w => w.AttackModifier);
+        public int TotalDefenseSkill => DefenseSkill + Inventory.OfType<Armor>().Sum(d => d.DefenseModifier);
+
         public List<Item> Inventory { get; set; } = new List<Item>();
         // Тук можем да добавим още свойства като магически точки, заклинания и т.н.
     }
